a840597ea34f633138a84c893c799496e9d23ebe
Robert B <robertb@lifesciencepublishers.com>
2020-11-05 11:22:14 -0700

    Optimized Solution

--- Arrays_Question_1/Two_Sum.js
+++ Arrays_Question_1/Two_Sum.js
@@ -1,8 +1,8 @@
 
 const nums = [1,3,7,2,9];
-const tragetToFind = 11;
+const tragetToFind = 3;
 
-// Brute Force Solution
+// Brute Force Solution Time = O(n2), Space = O(1);
 function findTwoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         console.log({i, value: nums[i]});
@@ -17,4 +17,23 @@ function findTwoSum(nums, target) {
     }
     return null;
 }
-console.log(findTwoSum(nums, tragetToFind)); 
+// console.log(findTwoSum(nums, tragetToFind));
+
+// Optimized Solution Time = O(n), Space = O(n);
+function findTwoSumB(nums, target) {
+    const numsMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        console.log({i, value: nums[i]});
+        const currentMapVal = numsMap[nums[i]];
+        if(currentMapVal >= 0){
+            return [currentMapVal, i];
+        } else {
+            const numberToFind = target - nums[i];
+            console.log({numberToFind});
+            numsMap[numberToFind] = i;
+            console.log({numsMap});
+        }
+    }
+    return null;
+}
+console.log(findTwoSumB(nums, tragetToFind));
