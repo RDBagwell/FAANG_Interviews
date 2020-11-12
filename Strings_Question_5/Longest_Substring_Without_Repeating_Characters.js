b79f854fe41485d0d139d5cf1ce4fa67ce1718d8
Robert B <robertb@lifesciencepublishers.com>
2020-11-12 12:46:29 -0700

    Question 5

--- Strings_Question_5/Longest_Substring_Without_Repeating_Characters.js
+++ Strings_Question_5/Longest_Substring_Without_Repeating_Characters.js
@@ -1,3 +1,26 @@
-const sting = "abcabcbb";
+const string = "abcabcbb";
 
-function lengthOfLongestSubstring(string){}
+// Brute Force Time: O(n^2), Space: O(n)
+function lengthOfLongestSubstring(s){
+  let longest  = 0;
+  if(s.length <=1){
+    return s.length;
+  }
+  for(let left = 0; left < s.length; left++){
+    let seenChars = {}, currentLength = 0;
+    for (let right = left; right < s.length; right++) {
+      const currentChar = s[right];
+      if(!seenChars[currentChar]){
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength)
+      } else {
+        break;
+      }
+    }
+  }
+    console.log(longest);
+}
+
+lengthOfLongestSubstring(string);
+
